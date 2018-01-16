60119685dfd6d90b04a787d653d293754abe6f95
zewlie@gmail.com <zewlie@gmail.com>
2018-01-16 09:10:32 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chrome extension
+# Replaces the innerHTML of the notifications li with a link to https://twitter.com/webdevMason/lists/shortlist/
+# Drag & drop folder into chrome://extensions/
+
+# Modified from: https://blog.lateral.io/2016/04/create-chrome-extension-modify-websites-html-css/
---
+++ content.js
@@ -0,0 +1,4 @@
+var li = document.getElementsByClassName('notifications');
+
+li[0].innerHTML = '<a role="button" href="/webdevMason/lists/shortlist/" class="js-nav js-tooltip js-dynamic-tooltip"><span class="Icon Icon--notifications Icon--large"></span><span class="text" aria-hidden="true">Shortlist</span><span class="u-hiddenVisually a11y-inactive-page-text">Shortlist</span><span class="u-hiddenVisually a11y-active-page-text">Shortlist, current page.</span></a>';
+
